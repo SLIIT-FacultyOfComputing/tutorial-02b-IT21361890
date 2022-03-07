1df6523dfce9f28b3a8f1d68216127410dc55f10
Ammaar M.N <99869139+IT21361890@users.noreply.github.com>
2022-03-07 17:56:44 +0000

    updated

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,48 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+#include <iostream>
+
+void Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+  std::cout << "Enter a value for n: ";
+  std::cin >> n;
+  std::cout << "Enter a value for r: ";
+  std::cin >> r;
+  std::cout << "nCr = ";
+  std::cout << nCr(n,r);
+  std::cout << std::endl;
+  return 0;
+}
+
+void Factorial(int no)//function factorial begins
+{
+  long fac =1;
+  for (int r=no; r >= 1; r--) {//for loop begins
+        fac = fac * r;
+    }
+}
+
+
+
+long nCr(int n, int r)// function nCr begins
+{
+  long fac1=1, fac2=1, fac3=1;
+  for (int i=n; i >= 1; i--) {//for loop begins
+        fac1 = fac1 * i;
+    }
+
+  for (int j=r; j >= 1; j--) {//for loop begins
+        fac2 = fac2 * j;
+    }
+
+  for (int k=(n-r); k >= 1; k--) {//for loop begins
+        fac3 = fac3 * (k);
+    }
+
+  return fac1/(fac2*fac3);
+}
+
